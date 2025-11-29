e9b07d793abb0fed9291d4c70d020b20e62e442a
Sriram-Dee <sriram14jeeva12@gmail.com>
2025-11-29 19:27:14 +0530

    Added smooth scroll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Portfolio from './pages/Portfolio';
-import Admin from './pages/Admin';
-import Login from './pages/Login';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Portfolio from "./pages/Portfolio";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SmoothScroll from "./components/SmoothScroll";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/login" element={<Login />} />
-        <Route 
-          path="/admin" 
-          element={
-            <ProtectedRoute>
-              <Admin />
-            </ProtectedRoute>
-          } 
-        />
-      </Routes>
+      <SmoothScroll>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </SmoothScroll>
     </Router>
   );
 }
